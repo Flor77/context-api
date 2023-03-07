1801bf36b4d81868fa8bfb21f640a440ebaa68e1
Flor77 <budaflorin@yahoo.com>
2023-03-07 20:59:07 +0200

    context api

---
+++ src/container/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../App.css";
+import { useUserContext } from "../context/userContext";
+
+const Header = () => {
+  const { user, logOut } = useUserContext();
+  return (
+    <div className="ui fixed menu ">
+      <div className="ui center">
+        <h2>React ContextApi</h2>
+
+        <div className="profile">
+          <h3>Welcome, {user.name}</h3>
+          {!user.isGuestUser && (
+            <button onClick={logOut} className="ui button blue">
+              LogOut
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
