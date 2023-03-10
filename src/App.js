fdc8dce73a0eb08364870f9c3361ba34d5eeb056
Flor77 <budaflorin@yahoo.com>
2023-03-10 18:38:05 +0200

    test2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import "./App.css";
-import Auth from "./container/Auth";
-import Header from "./container/Header";
-import { UserContextProvider } from "./context/userContext";
 
 function App() {
-  return (
-    <div className="ui container">
-      <UserContextProvider>
-        <Header />
-        <Auth />
-      </UserContextProvider>
-    </div>
-  );
+  return <div className="ui container"></div>;
 }
 
 export default App;
